dd05068a3872260eb23a0ea410d5aa1fbe6a44ad
Clu <retsulcconnor@gmail.com>
2020-12-13 12:54:44 -0500

    added inquirer prompt

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to the output!')
+// });
+
+
+
+
+// Material from earlier in the lesson
+// const profileDataArgs = process.argv.slice(2);
 
 // This one-liner can replace having to declare two separate variables 
 // e.g. const name = profileDataArgs[0]; & const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to the output!')
-});
+// const [name, github] = profileDataArgs;
